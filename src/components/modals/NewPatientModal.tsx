6f53a469beeb66036163686fea0e606e95442835
10sign <10signcom@gmail.com>
2025-09-17 13:22:40 +0200

    Fix AutoCapitalizeInput import

--- src/components/modals/NewPatientModal.tsx
+++ src/components/modals/NewPatientModal.tsx
@@ -9,6 +9,7 @@ import { Patient, PatientFormData, TreatmentHistoryEntry } from '../../types';
 import { validatePatientData } from '../../utils/validation';
 import { ConsultationService } from '../../services/consultationService';
 import AutoCapitalizeTextarea from '../ui/AutoCapitalizeTextarea';
+import AutoCapitalizeInput from '../ui/AutoCapitalizeInput';
 import { patientCache } from '../../utils/patientCache';
 import DocumentUploadManager from '../ui/DocumentUploadManager';
 import { DocumentMetadata } from '../../utils/documentStorage';
